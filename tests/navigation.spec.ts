8290c5c3cfa3984178fb9b74b35fecbd66821fef
Dan Burden <dan.burden@proton.me>
2025-03-12 20:50:17 +0000

    try to make nav test more resilient

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -13,27 +13,28 @@ import { test, expect } from "@playwright/test";
 // robert-to-browning-elizabeth-barrett./36113 |
 
 test("timeline content block link navigates to page", async ({
-	page,
-	context,
+    page,
+    context,
 }) => {
-	await page.goto("https://demo.quartexcollections.com/");
+    await page.goto("https://demo.quartexcollections.com/");
 
-	// expand the discovery aids block
-	await page.getByRole("button", { name: "Discovery Aids" }).click();
-	// click to navigate to Timeline item
-	await page.getByText("The Brownings: A Brief History").click();
-	await page.waitForURL(/\/discovery-aids\/the-brownings-a-brief-history/);
-	// set up expected new tab
-	const newTabPromise = context.waitForEvent("page");
-	// need to scroll to load content, so hook into the loading message
-	await page.getByText("Loading....").scrollIntoViewIfNeeded(); // this may not be robust
-	await page.getByRole("link", { name: "one of their first love" }).click();
-	// expect to open new tab
-	const newTab = await newTabPromise;
-	await newTab.waitForURL(
-		/\/Documents\/Detail\/10-january-1845\.-browning-robert-to-browning-elizabeth-barrett\.\/36113/,
-	);
-	await expect(newTab).toHaveTitle(
-		/\[10 January 1845\]\. Browning, Robert to Browning, Elizabeth Barrett\..*/, // trailing wildcard allows for other environments
-	); 
+    // expand the discovery aids block
+    await page.getByRole("button", { name: "Discovery Aids" }).click();
+    // click to navigate to Timeline item
+    await page.getByText("The Brownings: A Brief History").click();
+    await page.waitForURL(/\/discovery-aids\/the-brownings-a-brief-history/);
+    // set up expected new tab
+    const newTabPromise = context.waitForEvent("page");
+    // need to scroll to load content, so hook into the loading message
+    await page.getByText("Loading....").scrollIntoViewIfNeeded(); // this may not be robust
+    await page.waitForLoadState('domcontentloaded');
+    await page.getByRole("link", { name: "one of their first love" }).click();
+    // expect to open new tab
+    const newTab = await newTabPromise;
+    await newTab.waitForURL(
+        /\/Documents\/Detail\/10-january-1845\.-browning-robert-to-browning-elizabeth-barrett\.\/36113/,
+    );
+    await expect(newTab).toHaveTitle(
+        /\[10 January 1845\]\. Browning, Robert to Browning, Elizabeth Barrett\..*/, // trailing wildcard allows for other environments
+    );
 });
